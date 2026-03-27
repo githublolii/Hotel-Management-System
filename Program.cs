0185c07fb94a504dd5a5e735d0b4ed5fcf3ccfc3
Laila Awad <lailanihadawad@gmail.com>
2026-03-27 14:09:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using HotelManagement.Data;
+using HotelManagement.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer("Server=.\\SQLEXPRESS;Database=HotelManagementDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True"));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
